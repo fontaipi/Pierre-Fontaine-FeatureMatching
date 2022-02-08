2b63c331fb29e559c361f512a97b62f64d2f4ece
Pierre Fontaine <pierrefontaine00@gmail.com>
2022-02-08 08:52:12 +0100

    Init project

---
+++ src/Pierre.Fontaine.FeatureMatching/ObjectDetection.cs
@@ -0,0 +1,12 @@
+﻿namespace Pierre.Fontaine.FeatureMatching;
+
+public class ObjectDetection
+{
+
+    public IList<ObjectDetectionResult> DetectObjectInScenes(byte[]
+        objectImageData, IList<byte[]> imagesSceneData)
+    {
+        throw new NotImplementedException();
+    }
+
+}
